d5cd2e65e3b1ab462b373c9ad6e2616233ee7bd5
Vikhyath <vikhyath.kothamasu@gmail.com>
2021-06-26 17:19:51 +0530

    basic grid generation

--- Crossword.py
+++ Crossword.py
@@ -8,6 +8,8 @@ import re
 from discord.ext import commands
 from dotenv import load_dotenv
 import random as rd
+import cv2 as cv
+import numpy as np
 
 # url = "https://raw.githubusercontent.com/doshea/nyt_crosswords/master/2000/06/24.json"
 # response = requests.get(url)
@@ -33,8 +35,32 @@ class Crossword():
 		self.across_answers = self.json_data["answers"]["across"]
 		self.down_answers = self.json_data["answers"]["down"]
 		self.author = self.json_data["author"]
+		self.across_clues = self.json_data["clues"]["across"]
+		self.down_clues = self.json_data["clues"]["down"]
+		self.copyright = self.json_data["copyright"]
+		self.date = self.json_data["date"]
+		self.dow = self.json_data["dow"]
+		self.grid = self.json_data["grid"]
+		self.gridnums = self.json_data["gridnums"]
+		self.rows = int(self.json_data["size"]["rows"])
+		self.columns = int(self.json_data["size"]["cols"])
+		self.blank = np.zeros((self.rows*30 + (self.rows +1)*3, self.rows*30 + (self.rows +1)*3, 3), dtype='uint8')
+		self.blank[:,:] = 255, 255, 255
+		for i in range(0, self.rows+1):
+			cv.rectangle(self.blank, (33*i, 0), (33*i + 3, self.rows*30 + (self.rows +1)*3), (128, 128, 128), thickness=-1)
+			cv.rectangle(self.blank, (0, 33*i), (self.rows*30 + (self.rows +1)*3, 33*i + 3), (128, 128, 128), thickness=-1)
+		for count, value in enumerate(self.grid):
+			row = count % self.rows
+			col = count // self.rows
+			if(value == '.'):
+				cv.rectangle(self.blank, (row*33 + 3, col*33 + 3), (row*33 + 33, col*33 + 33), (0, 0, 0), thickness=-1)
 		
 # abc
-# c = Crossword()
-# print(c.url)
-# print(c.author)
+c = Crossword()
+print(c.url)
+print(c.author)
+print(c.grid)
+cv.imshow('grid', c.blank)
+cv.waitKey(0)
+# temp = np.zeros((13*30 + (13 -1)*3, 13*30 + (13 -1)*3, 3), dtype='uint8')
+# temp = 255, 255, 255
